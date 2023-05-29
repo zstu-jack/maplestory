134fa905c44ce6ac15e792917a2c87e851d80888
olaolaaaa <12997169+olaolaaaa@user.noreply.gitee.com>
2023-05-29 14:50:20 +0000

    update scripts/npc/1032100.js.
    Signed-off-by: olaolaaaa <12997169+olaolaaaa@user.noreply.gitee.com>

--- scripts/npc/1032100.js
+++ scripts/npc/1032100.js
@@ -46,7 +46,7 @@ function action(mode, type, selection) {
             cm.dispose();
             return;
         } else if (status == 2 && mode == 0) {
-            cm.sendNext("����" + item + "�ɲ�����. ����׼���ò��ϰɡ�");
+            cm.sendNext("制作" + item + "可不容易. 请带齐材料再来找我.");
             cm.dispose();
             return;
         }
@@ -56,24 +56,24 @@ function action(mode, type, selection) {
             status--;
         if (status == 0) {
             if (cm.getLevel() >= 40) {
-                cm.sendNext("�ǰ�...������Ů�����ʦ����Ů��Ӧ��ͬһ����ҳ�ʱ��ĽӴ�......�������õ����ҵ����ϣ��һ�����һ������ĵ��ߡ�");
+                cm.sendNext("是啊...我是妖精中的炼金术士。但是，妖精不应该与人类长期接触......像你这样强大的人类应该可以例外？如果你带来了材料，我可以为你制作魔法物品。");
             } else {
-                cm.sendOk("�ҿ�������ϡ���ҹ�����Ʒ�����ǣ��Ҳ����͸�İ���ˡ�");
+                cm.sendOk("我可以制作稀有珍贵的物品，但不会轻易向陌生人提供这项服务。");
                 cm.dispose();
             }
         } else if (status == 1) {
-            cm.sendSimple("����Ҫ��ʲô#b\\r\\n#L0#��ʯ#l\\r\\n#L1#��ʯ#l\\r\\n#L2#����ë#l");
+            cm.sendSimple("你想要做什么#b\r\n#L0#月石#l\r\n#L1#星石#l\r\n#L2#黑羽毛#l");
         } else if (status == 2) {
             selected = selection;
             if (selection == 0) {
-                item = "��ʯ";
-                cm.sendYesNo("����Ҫ��" + item + "? ��ô����Ҫ�Ĳ�����: #b#t4011000##k, #b#t4011001##k,\r\n#b#t4011002##k, #b#t4011003##k, #b#t4011004##k, #b#t4011005##k, �� #b#t4011006##k. Ȼ���� 10,000 ���");
+                item = "#v4011007##r#z4011007#";
+                cm.sendYesNo("所以，你想要做" + item + "? \r\n那么你需要的材料有: #b1个#v4011000##z4011000##k, #b1个#v4011001##z4011001##k, #b1个#v4011002##z4011002##k, #b1个#v4011003##z4011003##k, #b1个#v4011004##z4011004##k, #b1个#v4011005##z4011005##k, #b1个#v4011006##z4011006##k. 然后还有 10,000 金币");
             } else if (selection == 1) {
-                item = "��ʯ";
-                cm.sendYesNo("����Ҫ��" + item + "? ��ô����Ҫ�Ĳ�����: #b#t4021000##k, #b#t4021001##k, #b#t4021002##k, #b#t4021003##k, #b#t4021004##k, #b#t4021005##k, #b#t4021006##k, #b#t4021007##k �� #b#t4021008##k. Ȼ���� 15,000 ���");
+                item = "#v4021009##r#z4021009#";
+                cm.sendYesNo("所以，你想要做" + item + "? \r\n那么你需要的材料有: #b1个#v4021000##z4021000##k, #b1个#v4021001##z4021001##k, #b1个#v4021002##z4021002##k, #b1个#v4021003##z4021003##k, #b1个#v4021004##z4021004##k, #b1个#v4021005##z4021005##k, #b1个#v4021006##z4021006##k, #b1个#v4021007##z4021007##k, #b1个#v4021008##z4021008##k. 然后还有 15,000 金币");
             } else if (selection == 2) {
-                item = "����ë";
-                cm.sendYesNo("����Ҫ��" + item + "? ��ô����Ҫ�Ĳ�����: #b1 #t4001006##k, #b1 #t4001007##k �� #b1 #t4001008##k. Ȼ���� 30,000 ���");
+                item = "#v4031042##r#z4031042#";
+                cm.sendYesNo("所以，你想要做" + item + "? \r\n那么你需要的材料有: #b1个#v4001006##z4001006##k, #b1个#v4021008##z4021008##k, #b1个#v4021009##z4021009##k. 然后还有 30,000 金币");
             }
         } else if (status == 3) {
             if (selected == 0) {
@@ -83,9 +83,9 @@ function action(mode, type, selection) {
                         cm.gainItem(i,-1);
                     }
                     cm.gainItem(4011007, 1);
-                    cm.sendNext("��, �ú���� " + item + "�� �ҿ������˷ǳ��߼��Ĳ��ϰ������õġ����л�Ұɡ�");
+                    cm.sendNext("好了，拿着这个 " + item + "。它很不错吧？因为使用了优质的材料。如果还有什么需要帮忙的，随时来找我。");
                 } else {
-                    cm.sendNext("�������Ǯ�����ǲ��Ǳ��ˣ�Ǯ�����������ǣ�\r\n#r#t4011000##k\r\n#r#t4011001##k \r\n#r#t4011002##k \r\n#r#t4021003##k \r\n#r#t4011003##k \r\n#r#t4011005##k \r\n#r#t4011006##k\r\n���е�ĳһ�����ϲ�����");
+                    cm.sendNext("你确定带够了金币么？请检查一下，有没有带好#v4011000##z4011000#，#v4011001##z4011001#，#v4011002##z4011002#，#v4011003##z4011003#，#v4011004##z4011004#，#v4011005##z4011005#，#v4011006##z4011006#各一个。");
                 }
             } else if (selected == 1) {
                 if (cm.haveItem(4021000) && cm.haveItem(4021001) && cm.haveItem(4021002) && cm.haveItem(4021003) && cm.haveItem(4021004) && cm.haveItem(4021005) && cm.haveItem(4021006) && cm.haveItem(4021007) && cm.haveItem(4021008) && cm.getMeso() >= 15000) {
@@ -94,9 +94,9 @@ function action(mode, type, selection) {
                         cm.gainItem(j,-1);
                     }
                     cm.gainItem(4021009, 1);
-                    cm.sendNext("��, �ú���� " + item + "�� �ҿ������˷ǳ��߼��Ĳ��ϰ������õġ����л�Ұɡ�");
+                    cm.sendNext("好了，拿着这个 " + item + "。它很不错吧？因为使用了优质的材料。如果还有什么需要帮忙的，随时来找我。");
                 } else {
-                    cm.sendNext("�������Ǯ�����ǲ��Ǳ��ˣ���������ϸ����²����Ƿ��롣");
+                    cm.sendNext("你确定带够了金币么？请检查一下，有没有带好#v4021000##z4021000#，#v4021001##z4021001#，#v4021002##z4021002#，#v4021003##z4021003#，#v4021004##z4021004#，#v4021005##z4021005#，#v4021006##z4021006#，#v4021007##z4021007#，#v4021008##z4021008#各一个。");
                 }
             } else if (selected == 2) {
                 if (cm.haveItem(4001006) && cm.haveItem(4011007) && cm.haveItem(4021008) && cm.getMeso() >= 30000) {
@@ -105,9 +105,9 @@ function action(mode, type, selection) {
                         cm.gainItem(k,-1);
                     }
                     cm.gainItem(4031042, 1);
-                    cm.sendNext("�������ˣ��ú� " + item + "���л�Ұɡ�");
+                    cm.sendNext("好了，拿着这个 " + item + "。它很不错吧？因为使用了优质的材料。如果还有什么需要帮忙的，随时来找我。");
                 } else {
-                    cm.sendNext("�������Ǯ�����ǲ��Ǳ��ˣ���������ϸ����²����Ƿ��롣");
+                    cm.sendNext("你确定带够了金币么？请检查一下，有没有带好#v4001006##z4001006#，#v4011007##z4011007#，#v4021008##z4021008#各一个。");
                 }
             }
             cm.dispose();
